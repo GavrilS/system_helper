6131b8b7bab9562194bab1a289ec20e629bad816
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2023-08-27 12:54:32 +0300

    Added functionality to list all commands

--- core/test/driver_code.py
+++ core/test/driver_code.py
@@ -1,5 +1,6 @@
 from sys import platform
 import sys
+from factory.commands.windows_commands import Windows
 
 # for line in sys.path:
 #     print(line)
@@ -21,26 +22,29 @@ test_command = 'powershell -Command Get-Service'
 
 
 def main():
-    print(platform)
+    # print(platform)
 
-    connector = ConnectorFactory.get_connector()
-    # connector = ConnectorFactory.get_connector(key='windows', host='172.29.224.1', user='Gari', password='qwerty')
-    # connector = ConnectorFactory.get_connector(key='windows', host='172.29.224.1', user='Gari', password='')
-    output, err, _ = connector.execute_command(test_command)
-    print("********************")
-    print('Output: ', output)
-    print('********************')
-    print('Error: ', err)
-    print('********************')
-    print('Other: ', _)
+    # connector = ConnectorFactory.get_connector()
+    # # connector = ConnectorFactory.get_connector(key='windows', host='172.29.224.1', user='Gari', password='qwerty')
+    # # connector = ConnectorFactory.get_connector(key='windows', host='172.29.224.1', user='Gari', password='')
+    # output, err, _ = connector.execute_command(test_command)
+    # print("********************")
+    # print('Output: ', output)
+    # print('********************')
+    # print('Error: ', err)
+    # print('********************')
+    # print('Other: ', _)
 
-    with open('core\\test\\service_state.txt', 'w') as f:
-        f.write('Command output:' + '*'*40)
-        f.write(str(output))
-        f.write('Command error:' + '*'*40)
-        f.write(str(err))
-        f.write('Extra:' + '*'*40)
-        f.write(str(_))
+    # with open('core\\test\\service_state.txt', 'w') as f:
+    #     f.write('Command output:' + '*'*40)
+    #     f.write(str(output))
+    #     f.write('Command error:' + '*'*40)
+    #     f.write(str(err))
+    #     f.write('Extra:' + '*'*40)
+    #     f.write(str(_))
+
+    Windows.list_commands()
+
 
 
 
--- factory/commands/windows_commands.py
+++ factory/commands/windows_commands.py
@@ -3,26 +3,45 @@ from enum import Enum
 class Windows(Enum):
     
     SERVICES = [
-        'get all services',
-        'get service',
-        'get services sorted by status',
-        'get non-default windows services'
+        ('get all services', '--'),
+        ('get service', '--'),
+        ('get services sorted by status', '--'),
+        ('get non-default windows services', '--')
     ]
     SYSTEM_RESOURCES = [
-        'get resources',
-        'get memory',
-        'get cpu'
+        ('get resources', '--'),
+        ('get memory', '--'),
+        ('get cpu', '--')
     ]
     PROCESSES = [
-        'get processes',
-        'list processes by priority',
-        'show process owner'
+        ('get processes', '--'),
+        ('list processes by priority', '--'),
+        ('show process owner', '--')
     ]
     NETWORK = [
-        'get network configs'
+        ('get network configs', '--')
     ]
 
 
     @classmethod
     def list_commands(cls):
-        pass
+        # print('attr: value')
+        # for attr in cls.__dict__:
+        #     print(f"{attr}: {cls.__dict__[attr]}")
+
+        # print('Members: ')
+        # for attr in cls.__dict__['_member_names_']:
+        #     print(f"{attr}: {getattr(cls, attr)}")
+        
+        commands = []
+        for e in cls:
+            print('E: ', e)
+            for x in e.value:
+                commands.append(x[0] + ' : ' + x[1])
+
+        # commands2 = [x[0] + " : " + x[1] for x in e.value for e in cls]
+        # print('Commands2: ',commands2)
+        for c in commands:
+            print(c)
+
+        return commands
