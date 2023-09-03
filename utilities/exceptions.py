a34b29afda304d9ab2381461b0320511a880716c
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2023-09-03 19:54:33 +0300

    Added additional custom exception for passing too many arguments to a command

--- utilities/exceptions.py
+++ utilities/exceptions.py
@@ -1,4 +1,4 @@
-
+CMD_ARGS_ERROR = 'Too many arguments were passed for this command! Ask for help to see how the command is used!'
 
 class CommandNotFound(Exception):
     """
@@ -9,6 +9,20 @@ class CommandNotFound(Exception):
     """
 
     def __init__(self, message="The command does not exist!"):
+        self.message = message
+        super().__init__(self.message)
+
+
+
+class TooManyCommandArgs(Exception):
+    """
+    Exception raised when too many commands arguments are passed to a command.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message=CMD_ARGS_ERROR):
         self.message = message
         super().__init__(self.message)
         
